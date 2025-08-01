7ca00b3336792f946590124619c9afd5ffe5ba78
Hasan Qureshi <hasanq314@gmail.com>
2025-07-31 23:05:13 -0400

    Initial Commit

---
+++ backend/src/test/java/com/hasanq/workoutplanner/WorkoutplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasanq.workoutplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
