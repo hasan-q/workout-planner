54f754c459a91fe74ab64a6a906c712ca1288236
Hasan Qureshi <hasanq314@gmail.com>
2025-08-09 16:38:15 -0400

    Added refresh token

--- backend/src/main/java/com/hasanq/workoutplanner/model/RefreshToken.java
+++ backend/src/main/java/com/hasanq/workoutplanner/model/RefreshToken.java
@@ -1,9 +1,8 @@
 package com.hasanq.workoutplanner.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 
 public class RefreshToken {
 
@@ -14,5 +13,42 @@ public class RefreshToken {
     @Column(nullable = false, unique = true)
     private String token;
 
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private AppUser user;
 
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public AppUser getUser() {
+        return user;
+    }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
+
+    public Instant getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Instant expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
