c8413366aeb7bc1098e68d5cdc1d122589e658c8
Hasan Qureshi <hasanq314@gmail.com>
2025-08-10 16:52:43 -0400

    Created Workout Entry Service

--- backend/src/main/java/com/hasanq/workoutplanner/service/WorkoutEntryService.java
+++ backend/src/main/java/com/hasanq/workoutplanner/service/WorkoutEntryService.java
@@ -1,4 +1,46 @@
 package com.hasanq.workoutplanner.service;
 
+import com.hasanq.workoutplanner.model.AppUser;
+import com.hasanq.workoutplanner.model.Workout;
+import com.hasanq.workoutplanner.model.WorkoutEntry;
+import com.hasanq.workoutplanner.repository.WorkoutEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WorkoutEntryService {
+
+    private final WorkoutEntryRepository workoutEntryRepository;
+
+    @Autowired
+    public WorkoutEntryService(WorkoutEntryRepository workoutEntryRepository) {
+        this.workoutEntryRepository = workoutEntryRepository;
+    }
+
+    public List<WorkoutEntry> getAllEntriesByWorkout(Workout workout) {
+        return workoutEntryRepository.findByWorkout(workout);
+    }
+
+    public WorkoutEntry getEntryById(Long id) {
+        return workoutEntryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Workout Entry id=" + id + " not found."));
+    }
+
+    public WorkoutEntry createWorkoutEntry(Workout workout, WorkoutEntry workoutEntry) {
+        workoutEntry.setWorkout(workout);
+        return workoutEntryRepository.save(workoutEntry);
+    }
+
+    public WorkoutEntry updateWorkoutEntry(WorkoutEntry existingEntry, WorkoutEntry updatedEntry) {
+        existingEntry.setExercise(updatedEntry.getExercise());
+        existingEntry.setSets(updatedEntry.getSets());
+        existingEntry.setNotes(updatedEntry.getNotes());
+        return workoutEntryRepository.save(existingEntry);
+    }
+
+    public void deleteWorkoutEntry(WorkoutEntry workoutEntry) {
+        workoutEntryRepository.delete(workoutEntry);
+    }
 }
