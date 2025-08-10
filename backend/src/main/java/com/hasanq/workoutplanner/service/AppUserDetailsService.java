fcf2dd744330b9229534e221438ad980aa062b76
Hasan Qureshi <hasanq314@gmail.com>
2025-08-10 14:45:17 -0400

    App user service created, extends UserDetailsService

---
+++ backend/src/main/java/com/hasanq/workoutplanner/service/AppUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.hasanq.workoutplanner.service;
+
+import com.hasanq.workoutplanner.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    @Autowired
+    public AppUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found."));
+    }
+}
