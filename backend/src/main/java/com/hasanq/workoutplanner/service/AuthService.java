495aa61e1cc1dd008547141c2ef131bc40a0cb77
Hasan Qureshi <hasanq314@gmail.com>
2025-08-11 18:12:11 -0400

    AuthService created

--- backend/src/main/java/com/hasanq/workoutplanner/service/AuthService.java
+++ backend/src/main/java/com/hasanq/workoutplanner/service/AuthService.java
@@ -1,14 +1,65 @@
 package com.hasanq.workoutplanner.service;
 
+import com.hasanq.workoutplanner.dto.AuthResponse;
+import com.hasanq.workoutplanner.dto.LoginRequest;
+import com.hasanq.workoutplanner.dto.RegisterRequest;
+import com.hasanq.workoutplanner.model.AppUser;
+import com.hasanq.workoutplanner.model.RefreshToken;
 import com.hasanq.workoutplanner.repository.AppUserRepository;
 import com.hasanq.workoutplanner.security.JwtService;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
+
     private final AppUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authManager;
     private final RefreshTokenService refreshTokenService;
+
+    public AuthService(
+            AppUserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService,
+            AuthenticationManager authManager,
+            RefreshTokenService refreshTokenService
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authManager = authManager;
+        this.refreshTokenService = refreshTokenService;
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        AppUser user = new AppUser();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        AppUser savedUser = userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(savedUser);
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(savedUser);
+
+        return new AuthResponse(jwtToken, refreshToken.getToken());
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        AppUser user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User with email " + request.getEmail() + " not found."));
+
+        String jwtToken = jwtService.generateToken(user);
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(user);
+
+        return new AuthResponse(jwtToken, refreshToken.getToken());
+    }
 }
