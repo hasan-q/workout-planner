04f150f59b8acd386240bd7b1b0f6ea5287b7a98
Hasan Qureshi <hasanq314@gmail.com>
2025-08-17 12:05:21 -0400

    Added Repository annotation

--- backend/src/main/java/com/hasanq/workoutplanner/repository/AppUserRepository.java
+++ backend/src/main/java/com/hasanq/workoutplanner/repository/AppUserRepository.java
@@ -2,9 +2,11 @@ package com.hasanq.workoutplanner.repository;
 
 import com.hasanq.workoutplanner.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 }
--- backend/src/main/java/com/hasanq/workoutplanner/repository/WorkoutRepository.java
+++ backend/src/main/java/com/hasanq/workoutplanner/repository/WorkoutRepository.java
@@ -3,9 +3,11 @@ package com.hasanq.workoutplanner.repository;
 import com.hasanq.workoutplanner.model.AppUser;
 import com.hasanq.workoutplanner.model.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     List<Workout> findByUser(AppUser user);
 }
