9a3b60a46558e85f59323958050c129e12353e2b
Hasan Qureshi <hasanq314@gmail.com>
2025-09-04 21:35:16 -0400

    Added register functionality to authService

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -11,3 +11,14 @@ export async function login({ email, password }) {
     return response.data;
 }
 
+export async function register({ username, email, password }) {
+    const response = await api.post("/auth/register", { username, email, password });
+
+    if (response.data.accessToken && response.data.refreshToken) {
+        saveTokens(response.data.accessToken, response.data.refreshToken);
+    }
+
+    return response.data;
+}
+
+
