3031425432a0c5c5ef986e7ba658193673b2a824
Hasan Qureshi <hasanq314@gmail.com>
2025-09-04 21:27:18 -0400

    Added error handling to api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getAccessToken } from "./tokenService";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "http://localhost:8080/api",
+    timeout: 10000
 });
 
 api.interceptors.request.use(config => {
@@ -13,5 +14,14 @@ api.interceptors.request.use(config => {
     }
     return config;
 });
+
+api.interceptors.response.use(response => response,
+    async error => {
+        if (error.response && error.response.status === 401) {
+            console.warn("Unauthorized Request");
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
