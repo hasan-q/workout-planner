2a2c99d89974013510e8a6da7c0f4a8aeb7ad9ce
Hasan Qureshi <hasanq314@gmail.com>
2025-09-05 13:17:38 -0400

    Created REST API endpoints for exerciseService

--- frontend/src/api/exerciseService.js
+++ frontend/src/api/exerciseService.js
@@ -1,1 +1,31 @@
 import api from "./api";
+
+export async function getExercises() {
+    const response = await api.get("/exercises");
+    return response.data;
+}
+
+export async function createExercise({ name, muscleGroup }) {
+    const body = {
+        name,
+        muscleGroup
+    }
+
+    const response = await api.post("/exercises", body);
+    return response.data;
+}
+
+export async function updateExercise(exerciseId, { name, muscleGroup }) {
+    const body = {
+        name,
+        muscleGroup
+    }
+
+    const response = await api.put(`/exercises/${exerciseId}`, body);
+    return response.data;
+}
+
+export async function deleteExercise(exerciseId) {
+    const response = await api.delete(`/exercises/${exerciseId}`);
+    return response.data;
+}
