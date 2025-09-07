61bbbd539f8c0c088259f3c6c1e061f678f2a9af
Hasan Qureshi <hasanq314@gmail.com>
2025-09-07 18:45:04 -0400

    Added async function in ExercisesPage to fetch all exercises from API

--- frontend/src/pages/ExercisesPage.jsx
+++ frontend/src/pages/ExercisesPage.jsx
@@ -1,3 +1,25 @@
 import { useState, useEffect } from "react";
 import { getExercises } from "../api/exerciseService";
+import ExerciseForm from "../components/ExercisesForm";
+import ExercisesList from "../components/ExercisesList";
 
+export default function ExercisesPage() {
+    const [exercises, setExercises] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [selectedExercise, setSelectedExercises] = useState(null);
+
+    useEffect(
+        () => {
+            (async () => {
+                try {
+                    const res = await getExercises();
+                    setExercises(res.data);
+                } catch (error) {
+                    console.error(error)
+                } finally {
+                    setLoading(false);
+                }
+            })();
+        }, []
+    );
+}
