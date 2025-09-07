11a73b8f1e48da0d29937ea8ea88bcf3402374cc
Hasan Qureshi <hasanq314@gmail.com>
2025-09-07 18:24:52 -0400

    Working ExerciseForm with muscleGroup and functional button text

--- frontend/src/components/ExercisesForm.jsx
+++ frontend/src/components/ExercisesForm.jsx
@@ -4,6 +4,13 @@ export default function ExerciseForm({ exercise, onSubmit, onClose }) {
     const [name, setName] = useState("");
     const [muscleGroup, setMuscleGroup] = useState("");
 
+    let buttonText;
+    if (exercise) {
+        buttonText = "Update Exercise";
+    } else {
+        buttonText = "Add Exercise";
+    }
+
     useEffect(
         () => {
             if (exercise) {
@@ -23,11 +30,30 @@ export default function ExerciseForm({ exercise, onSubmit, onClose }) {
 
         // passed through
         onSubmit(exerciseData);
+        setName("");
+        setMuscleGroup("");
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={name} placeholder="Exercise Name" onChange={(e) => setName(e.target.value)} required />
+            <input 
+                type="text" 
+                value={name} 
+                placeholder="Exercise Name" 
+                onChange={(e) => setName(e.target.value)} 
+                required 
+            />
+            <input
+                type="text"
+                value={muscleGroup}
+                placeholder="Muscle Group"
+                onChange={(e) => setMuscleGroup(e.target.value)}
+                required
+            />
+            <button type="submit">{buttonText}</button>
+            {exercise ? (
+                <button onClick={onClose}>Cancel</button>
+            ) : null}
         </form>
     );
 }
