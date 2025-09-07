584f3b9e2018539573f5e944e6ab08f84802f64a
Hasan Qureshi <hasanq314@gmail.com>
2025-09-07 17:01:12 -0400

    Added functionality to ExerciseList

--- frontend/src/components/ExercisesList.jsx
+++ frontend/src/components/ExercisesList.jsx
@@ -13,6 +13,16 @@ export default function ExercisesList({ exercises, onEdit, onDelete }) {
                         </tr>
                     </thead>
                     <tbody>
+                        {exercises.map((exercise) => {
+                            <tr key={exercise.id}>
+                                <td>{exercise.name}</td>
+                                <td>{exercise.muscleGroup}</td>
+                                <td>
+                                    <button onClick={() => onEdit(exercise)}></button>
+                                    <button onClick={() => onDelete(exercise.id)}></button>
+                                </td>
+                            </tr>
+                        })}
                     </tbody>
                 </table>
             )}
