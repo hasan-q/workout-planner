86876e543c06fe09bf56a7a9e0f47d067a653090
Hasan Qureshi <hasanq314@gmail.com>
2025-09-07 18:13:11 -0400

    added Exercise to exerciseForm

--- frontend/src/components/ExercisesForm.jsx
+++ frontend/src/components/ExercisesForm.jsx
@@ -1,1 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+export default function ExerciseForm({ exercise, onSubmit, onClose }) {
+    const [name, setName] = useState("");
+    const [muscleGroup, setMuscleGroup] = useState("");
+
+    useEffect(
+        () => {
+            if (exercise) {
+                setName(exercise.name);
+                setMuscleGroup(exercise.muscleGroup);
+            }
+        }, [exercise]
+    );
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let exerciseData = { name, muscleGroup }
+
+        if (exercise) {
+            exerciseData.id = exercise.id;
+        }
+
+        // passed through
+        onSubmit(exerciseData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={name} placeholder="Exercise Name" onChange={(e) => setName(e.target.value)} required />
+        </form>
+    );
+}
