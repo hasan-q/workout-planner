d689d53917416e0257875dd08fb64339b922f490
Hasan Qureshi <hasanq314@gmail.com>
2025-09-06 20:55:25 -0400

    Working register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,2 +1,31 @@
 import { useNavigate } from "react-router-dom";
 import { register } from "../api/authService";
+
+export default function RegisterPage() {
+    const navigate = useNavigate();
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const username = e.target.username.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        try {
+            await register({ username, email, password });
+            navigate("/dashboard")
+        } catch (error) {
+            alert("Registration failed, please try again.");
+            console.error(error);
+        }
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="text" name="username" required />
+            <input type="email" name="email" required />
+            <input type="password" name="password" required />
+            <button type="submit">Sign Up</button>
+        </form>
+    )
+}
