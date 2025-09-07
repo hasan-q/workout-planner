c2c5767d78d68eba83c20e4ab1a901c506e62859
Hasan Qureshi <hasanq314@gmail.com>
2025-09-07 19:54:03 -0400

    Reworked exercise pages

--- frontend/src/pages/ExercisesPage.jsx
+++ frontend/src/pages/ExercisesPage.jsx
@@ -59,7 +59,7 @@ export default function ExercisesPage() {
                     onDelete={handleDeleteExercise}
                 />
             )}
-            
+
             {selectedExercise && (
                 <ExerciseForm
                     exercise={selectedExercise}
