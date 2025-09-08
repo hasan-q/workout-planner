455f34dad4c682f3be3815a6552c86fcd1d51ff5
Hasan Qureshi <hasanq314@gmail.com>
2025-09-08 12:34:06 -0400

    Added Dashboard, added App imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,8 @@
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ExercisesPage from "./pages/ExercisesPage";
 
 export default function App() {
 }
