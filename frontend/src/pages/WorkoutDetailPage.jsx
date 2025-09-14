5001dd96d2ca1cb801a96b7612c642e28aa2d950
Hasan Qureshi <hasanq314@gmail.com>
2025-09-14 13:25:27 -0400

    Workout and exercise fetching completed, update workout completed

--- frontend/src/pages/WorkoutDetailPage.jsx
+++ frontend/src/pages/WorkoutDetailPage.jsx
@@ -8,9 +8,43 @@ import {
     deleteWorkoutEntry,
     createExerciseSet,
     updateExerciseSet,
-    deleteExerciseSet    
+    deleteExerciseSet
 } from "../api/workoutService";
+import { getExercises } from "../api/exerciseService";
 
 export default function WorkoutDetailPage() {
+    const { workoutId } = useParams();
+    const [workout, setWorkout] = useState(null);
+    const [exercises, setExercises] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(
+        () => {
+            (async () => {
+                try {
+                    const [fetchedWorkout, fetchedExercises] = await Promise.all([
+                        getWorkoutById(workoutId),
+                        getExercises()
+                    ]);
+                    setWorkout(fetchedWorkout);
+                    setExercises(fetchedExercises);
+                } catch(error) {
+                    console.error(error);
+                } finally {
+                    setLoading(false);
+                }
+            })();
+        }, [workoutId]
+    );
+
+    const handleUpdateWorkout = async () => {
+        const data = {
+            name: workout.name,
+            date: workout.date
+        };
+        const updatedWorkout = await updateWorkout(workoutId, data);
+        setWorkout(updatedWorkout);
+    };
+
 
 }
