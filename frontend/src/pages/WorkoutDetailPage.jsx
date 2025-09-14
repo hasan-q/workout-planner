1494826019e46166fa590d3968164ccf41ccb826
Hasan Qureshi <hasanq314@gmail.com>
2025-09-14 12:19:17 -0400

    Imports created for WorkoutDetail

--- frontend/src/pages/WorkoutDetailPage.jsx
+++ frontend/src/pages/WorkoutDetailPage.jsx
@@ -1,1 +1,11 @@
 import { useState, useEffect } from "react";
+import { 
+    getWorkoutById,
+    updateWorkout,
+    createWorkoutEntry,
+    updateWorkoutEntry,
+    deleteWorkoutEntry,
+    createExerciseSet,
+    updateExerciseSet,
+    deleteExerciseSet    
+} from "../api/workoutService";
