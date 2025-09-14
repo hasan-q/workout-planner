7a85c2ee520afabdf9c1fe5881bacdf23b44de5a
Hasan Qureshi <hasanq314@gmail.com>
2025-09-14 12:24:25 -0400

    Added workout detail route to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,14 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ExercisesPage from "./pages/ExercisesPage";
 import Dashboard from "./pages/Dashboard";
+import WorkoutDetailPage from "./pages/WorkoutDetailPage";
 
 // Unprotected Routes: Login, Register
-// Protected Routes: Dashboard, Exercises, Workouts
+// Protected Routes: Dashboard, Exercises, Workouts, Workout details page, History
 
 export default function App() {
     return (
         <Routes>
-
             <Route path="/" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
 
@@ -19,6 +19,7 @@ export default function App() {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/exercises" element={<ExercisesPage />} />
                 <Route path="/workouts" element={<WorkoutsPage />} />
+                <Route path="/workouts/:workoutId" element={<WorkoutDetailPage />} />
             </Route>
         </Routes>
     );
--- frontend/src/pages/WorkoutDetailPage.jsx
+++ frontend/src/pages/WorkoutDetailPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { 
     getWorkoutById,
     updateWorkout,
@@ -9,3 +10,7 @@ import {
     updateExerciseSet,
     deleteExerciseSet    
 } from "../api/workoutService";
+
+export default function WorkoutDetailPage() {
+
+}
