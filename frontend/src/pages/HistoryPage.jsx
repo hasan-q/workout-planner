0a416b8543c800bec55b1a41d130acc529907f2d
Hasan Qureshi <hasanq314@gmail.com>
2025-09-16 21:14:16 -0400

    Fetch workouts for history added

--- frontend/src/pages/HistoryPage.jsx
+++ frontend/src/pages/HistoryPage.jsx
@@ -1,3 +1,29 @@
 import { useState, useEffect } from "react";
 import { getWorkouts } from "../api/workoutService";
 
+export default function HistoryPage() {
+    const [workouts, setWorkouts] = useState([]);
+    const [expandedWorkout, setExpandedWorkout] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(
+        () => {
+            (async () => {
+                try {
+                    const allWorkouts = await getWorkouts();
+                    const startedWorkouts = allWorkouts.filter(workout => workout.date);
+                    setWorkouts(startedWorkouts);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setLoading(false);
+                }
+            })();
+        }, []
+    );
+
+    const toggleExpanded = (id) => {
+        // only one workout can be toggled at a time, uses ID
+        setExpandedWorkout(expandedWorkout === id ? null : id);
+    };
+}
