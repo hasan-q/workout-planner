6a7c87e8f221b4ff660c6928d3ed152058bd6d2c
Hasan Qureshi <hasanq314@gmail.com>
2025-10-15 15:09:11 -0400

    Moved userTemplate logic to seperate hook

---
+++ frontend/src/hooks/useUserTemplates.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { getWorkouts } from "../api/workoutService";
+
+export function useUserTemplates(limit = null) {
+    const [templates, setTemplates] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(
+        () => {
+            (async () => {
+                try {
+                    const allWorkouts = await getWorkouts();
+                    const templates = allWorkouts.filter(workout => !workout.date);
+                    setTemplates(limit ? templates.slice(0, limit) : templates);
+                } catch (error) {
+                    console.log(error);
+                    setError(error);
+                } finally {
+                    setLoading(false);
+                }
+            })();
+        }, [limit]
+    );
+
+    return { templates, loading, error };
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,18 @@
 import "../components/pages.css";
+import { useUserTemplates } from "../hooks/useUserTemplates";
 
 export default function Dashboard() {
 
     const username = localStorage.getItem("username");
+    const { templates, loading } = useUserTemplates(5);
+
+    if (loading) return <p>Loading...</p>
 
     return (
         <div className="dashboard">
             <h1>Hello, {username}</h1>
             <h2 className="title-text">Your Templates</h2>
+            
         </div>
     );
 }
