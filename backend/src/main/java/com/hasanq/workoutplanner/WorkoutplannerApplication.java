25598303153715293534699b3c469d3a1ed328ab
Hasan Qureshi <hasanq314@gmail.com>
2025-10-15 18:23:16 -0400

    chore: clean up whitespace in WorkoutplannerApplication.java

--- backend/src/main/java/com/hasanq/workoutplanner/WorkoutplannerApplication.java
+++ backend/src/main/java/com/hasanq/workoutplanner/WorkoutplannerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorkoutplannerApplication {	
+public class WorkoutplannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutplannerApplication.class, args);
