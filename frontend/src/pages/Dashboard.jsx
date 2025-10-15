be73eea6dc3919c0ffa16c36d96b303c829a7905
Hasan Qureshi <hasanq314@gmail.com>
2025-10-15 15:21:56 -0400

    Added templates to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,12 @@
 import "../components/pages.css";
+import "../components/dashboard.css";
 import { useUserTemplates } from "../hooks/useUserTemplates";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
 
     const username = localStorage.getItem("username");
+    const navigate = useNavigate();
     const { templates, loading } = useUserTemplates(5);
 
     if (loading) return <p>Loading...</p>
@@ -12,7 +15,20 @@ export default function Dashboard() {
         <div className="dashboard">
             <h1>Hello, {username}</h1>
             <h2 className="title-text">Your Templates</h2>
-            
+            {templates.length === 0 ? (
+                <p>You have no templates yet. </p>
+            ) : (
+                <ul className="dashboard-templates">
+                    {templates.map(template => (
+                        <li
+                            key={template.id}
+                            className="dashboard-template-item"
+                        >
+                            <h3>{template.name}</h3>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
